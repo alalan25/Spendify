611542ed9037ffabb1c04df48bc1551d172b4ec9
EricKogut <eric.jkogut@gmail.com>
2020-11-21 02:25:08 -0500

    Creating new components

--- frontend/spendify-app/src/app/app.module.ts
+++ frontend/spendify-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { YourAccountComponent } from './your-account/your-account.component';
+import { GlanceComponent } from './glance/glance.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    YourAccountComponent,
+    GlanceComponent,
+    LeaderboardComponent
   ],
   imports: [
     BrowserModule,
